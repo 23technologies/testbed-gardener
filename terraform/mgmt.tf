11a8b0fb54f72829dcb3779a9fbc970200971c70
Malte Münch <muench@23technologies.cloud>
2020-11-12 12:27:15 +0100

    Terraform fmt: XS
    Signed-off-by: Malte Münch <muench@23technologies.cloud>

--- terraform/mgmt.tf
+++ terraform/mgmt.tf
@@ -57,7 +57,7 @@ EOT
   }
 
   provisioner "file" {
-    content      = templatefile("files/${var.cloud_provider}/acre.yaml.tmpl", {clouds = yamldecode(file("clouds.yaml")), secure = yamldecode(file("secure.yaml"))})
+    content     = templatefile("files/${var.cloud_provider}/acre.yaml.tmpl", { clouds = yamldecode(file("clouds.yaml")), secure = yamldecode(file("secure.yaml")) })
     destination = "/home/${var.ssh_username}/acre.yaml"
   }
 
