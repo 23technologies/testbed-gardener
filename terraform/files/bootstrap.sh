1b3ab50baffff5be6e31f602ae4cc88df6c4ef3d
Malte Münch <muench@23technologies.cloud>
2020-12-08 17:29:38 +0100

    Quoting variables...
    Signed-off-by: Malte Münch <muench@23technologies.cloud>

--- terraform/files/bootstrap.sh
+++ terraform/files/bootstrap.sh
@@ -7,12 +7,12 @@ VERSION_K9S=0.22.1
 VERSION_RKE=1.2.3
 
 
-chmod 0600 $HOME/.ssh/id_rsa
+chmod 0600 "$HOME/.ssh/id_rsa"
 sudo apt-get install -y git
 
 sudo snap install kubectl --classic
 
-sudo /usr/bin/wget -O /usr/local/bin/rke https://github.com/rancher/rke/releases/download/v$VERSION_RKE/rke_linux-amd64
+sudo /usr/bin/wget -O "/usr/local/bin/rke https://github.com/rancher/rke/releases/download/v$VERSION_RKE/rke_linux-amd64"
 sudo chmod +x /usr/local/bin/rke
 rke up
 mkdir -p "$HOME/.kube"
@@ -31,12 +31,12 @@ echo "alias k=kubectl" >> ~/.bashrc
 
 # install gardenctl
 
-sudo curl -L -o /usr/local/bin/gardenctl https://github.com/garden-cluster/gardenctl/releases/download/v$VERSION_GARDENCTL/gardenctl-linux-amd64
+sudo curl -L -o /usr/local/bin/gardenctl https://github.com/garden-cluster/gardenctl/releases/download/v"$VERSION_GARDENCTL"/gardenctl-linux-amd64
 sudo chmod +x /usr/local/bin/gardenctl
 
 # install k9s
 
-curl -L https://github.com/derailed/k9s/releases/download/v$VERSION_K9S/k9s_Linux_x86_64.tar.gz | sudo tar xzf - -C /usr/local/bin/
+curl -L https://github.com/derailed/k9s/releases/download/v"$VERSION_K9S"/k9s_Linux_x86_64.tar.gz | sudo tar xzf - -C /usr/local/bin/
 sudo chmod +x /usr/local/bin/k9s
 
 # install sow
